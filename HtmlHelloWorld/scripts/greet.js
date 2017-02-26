5fba78af09e64cd7d4e4e7b1e48767f4426dfaf0
Celeste van Wyk <celestvw2@gmail.com>
2017-02-26 08:36:50 +0200

    Added styling

--- HtmlHelloWorld/scripts/greet.js
+++ HtmlHelloWorld/scripts/greet.js
@@ -1,19 +1,4 @@
-﻿(function () {
-    'use strict';
-
-    angular
-        .module('app')
-        .controller('controller1', controller1);
-
-    controller1.$inject = ['$location']; 
-
-    function controller1($location) {
-        /* jshint validthis:true */
-        var vm = this;
-        vm.title = 'controller1';
-
-        activate();
-
-        function activate() { }
-    }
-})();
+﻿function greetUser() {
+    var firstName = prompt('Please tell me your name');
+    alert("Hello " + firstName + "!");
+}
